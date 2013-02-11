400218235458b63e40dd0a3228845d4b638dcd71
T. Colin Strong <turvyc@gmail.com>
2013-02-11 15:24:11 -0800

    Forgot to add index.php and info.php to the last commit. Sorry. They are included in this commit.

---
+++ server/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>PeaceGuard</title>
+    </head>
+    <body>
+        <h1>PeaceGuard</h1>
+        <p><a href="info.php">View PHP Info</a></p>
+    </body>
+</html>
