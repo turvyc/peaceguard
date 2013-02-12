3979e2234317610048ff0420a84ee423547cb18d
T. Colin Strong <cstrong@sfu.ca>
2013-02-12 12:57:00 -0800

    Added getter functions to Volunteer class.

--- server/model/volunteer.model.php
+++ server/model/volunteer.model.php
@@ -12,5 +12,39 @@ require_once('user.model.php');
 
 class VolunteerModel extends UserModel {
 
+    private $joinDate;
+    private $numberOfReports;
+    private $numberOfPatrols;
+    private $totalTime;
+    private $totalDistance;
+    // private $score;  Commented because we have no score algorithm as of yet
+
+    public function getJoinDate() {
+        return $this->joinDate;
+    }
+
+    public function getNumberOfReports() {
+        return $this->numberOfReports;
+    }
+
+    public function getNumberOfPatrols() {
+        return $this->numberOfPatrols;
+    }
+
+    public function getTotalTime() {
+        return $this->totalTime;
+    }
+
+    public function getTotalDistance() {
+        return $this->totalDistance;
+    }
+
+    public function getAverageTime() {
+        return $this->totalTime / $this->numberOfPatrols;
+    }
+
+    public function getAverageDistance() {
+        return $this->totalDistance / $this->numberOfPatrols;
+    }
 
 }
