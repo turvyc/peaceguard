611dc676ae7835a1332c98b7ca025c045c4bec16
Robert Sanchez <rcsanche@sfu.ca>
2013-02-12 23:38:44 -0800

    added starting details to construct fxn, and added reportResolved fxn

--- server/control/report.php
+++ server/control/report.php
@@ -8,9 +8,26 @@ The Report class.
 
 class Report {
 
+	private $rId;
+	private $rTime;
+	private $resolved;
+	private $location;
+	private $description;
+	private $severity;
+	//picture
+
+
     function __construct() {
-        // do something
+		$timestamp=getdate();			//date array
+		
+		$this->resolved=false;
+		$this->rTime=$timestamp[0];		//unix time of date array
+		// do something
     }
+	
+	function reportResolved(){
+		$this->resolved=true;
+	}
 
     // blah
 
