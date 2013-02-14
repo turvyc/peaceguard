b96da8d8967a7874c19c290bd48db9071e7a9e34
Robert Sanchez <rcsanche@sfu.ca>
2013-02-13 18:07:15 -0800

    fixed? report.php __construct

--- server/control/report.php
+++ server/control/report.php
@@ -6,22 +6,37 @@ The Report class.
 
 */
 
-class Report {
+require_once('../model/session.model.php');
+require_once('../model/constants.model.php');
+require_once('../model/datainterface.model.php');
 
-	private $rId;
-	private $rTime;
-	private $resolved;
-	private $location;
-	private $description;
-	private $severity;
+class Report {
+			
+	private $resolved;			//bool
+	private $r_id;				//maybe concatenate #ofReports.#ofPatrols.userId
+	private $r_time;			//
+	private $r_type;			//incident type
+	private $r_severity;		//
+	private $r_location;
+	private $r_description;
+	
 	//picture
 
-
     function __construct() {
-		$timestamp=getdate();			//date array
+	
+		$resolved=FALSE;
+		$timeArray=$getdate();
+		$r_time=timeArray[0];
 		
-		$this->resolved=false;
-		$this->rTime=$timestamp[0];		//unix time of date array
+		$r_type=$_GET["type"];
+		$r_severity=$_GET["serverity"];
+		$r_location=$_GET["location"];
+		$r_description=$_GET["description"];
+		
+		//|resolved tick box|reportId|reportTime|reportType|severity|location|description| - webportal display
+		$reportArray=array($resolved,$r_id,$r_time,$r_type,$r_severity,$r_location,$r_description);
+		
+		echo json_encode($reportArray);
 		// do something
     }
 	
