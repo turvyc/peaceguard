7c3323a977bf4b630e82cec94d09778d5e8d51ab
T. Colin Strong <turvyc@gmail.com>
2013-02-14 12:23:00 -0800

    Rewrote index.php to make use of a header and footer.

--- server/index.php
+++ server/index.php
@@ -1,9 +1,21 @@
-<html>
-    <head>
-        <title>PeaceGuard</title>
-    </head>
-    <body>
-        <h1>PeaceGuard</h1>
-        <p><a href="info.php">View PHP Info</a></p>
-    </body>
-</html>
+<?php
+
+include('header.html');
+include('model/session.model.php');
+
+// This should be in header.html, which would be changed to header.php
+$session = new Session();
+
+if (! $session->getUsername()) {
+    header('location:login.php');
+}
+
+?>
+
+<h2>Main page stuff goes here</h2>
+
+<?php
+
+include('footer.html);
+
+?>
