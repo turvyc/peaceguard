f824a4b118dead9741e6b6b3a218041752c9f5d1
Lesperance Ashley <alespera@sfu.ca>
2013-02-13 19:11:29 -0800

    added storyboard and viewcontroller

---
+++ app/peaceguard/peaceguard/PeaceGuardViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeaceGuardViewController.h
+//  peaceguard
+//
+//  Created by Lesperance Ashley on 2013-02-13.
+//  Copyright (c) 2013 Ashley Lesperance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeaceGuardViewController : UIViewController
+
+@end
