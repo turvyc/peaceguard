dfb805a4c468040ed759f44f568e7d9f60c4a8fe
T. Colin Strong <turvyc@gmail.com>
2013-02-14 14:04:43 -0800

    Fixed HTML form in login.php.

--- server/login.php
+++ server/login.php
@@ -1,11 +1,9 @@
 <?php include('header.html'); ?>
 
-<form action="control/login.control.php" method="POST">
-    USERNAME: <input type="text" name="username" >
-    <br />
-    PASSWORD: <input type="password" name="password" >
-    <br />
-    <input type="button" value="Login" onClick="Login(this.form);" >
+<form name="login" action="control/login.control.php" method="POST">
+    USERNAME: <input type="text" name="username" > <br />
+    PASSWORD: <input type="password" name="password" > <br />
+    <input type="submit" value="Login">
 </form>
 
 <?php
