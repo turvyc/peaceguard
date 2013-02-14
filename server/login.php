b56cb83cb0f61e1b18bbba339f7763e4a09ae8fb
T. Colin Strong <turvyc@gmail.com>
2013-02-14 14:11:42 -0800

    Fixed some tag issues in login.php.

--- server/login.php
+++ server/login.php
@@ -1,10 +1,10 @@
 <?php include('header.html'); ?>
 
 <form name="login" action="control/login.control.php" method="POST">
-    USERNAME: <input type="text" name="<?php echo _USERNAME; ?>" > <br />
-    PASSWORD: <input type="password" name="<?php echo _PASSWORD; ?>" > <br />
-    <input type="hidden" name="<?php echo _AGENT; ?>"
-    <input type="submit" value="Login">
+    USERNAME: <input type="text" name="<?php echo _USERNAME; ?>" /> <br />
+    PASSWORD: <input type="password" name="<?php echo _PASSWORD; ?>" /> <br />
+    <input type="hidden" name="<?php echo _AGENT; ?>" />
+    <input type="submit" value="Login" />
 </form>
 
 <?php
