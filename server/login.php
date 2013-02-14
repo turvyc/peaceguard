35fb9e2b38fcdd58b30aa06269e5496473119856
T. Colin Strong <turvyc@gmail.com>
2013-02-14 12:54:55 -0800

    Changed control/login.php to control/login.control.php, to avoid confusion with login.php.

--- server/login.php
+++ server/login.php
@@ -1,51 +1,8 @@
-<?php
+<?php include('header.html'); ?>
 
-include('header.html');
- <html>
- <head>
- <title>Vancouver Police Department Peace Guard | Vancouver Police Department
- </title>
- <link rel="stylesheet" href="police.css" type="text/css" media="screen" />
- <link rel="stylesheet" href="police1.css" type="text/css" media="screen" >
- <link rel="stylesheet" href="police2.css" type="text/css" media="screen" >
- 
- <script language="JavaScript" type="text/JavaScript" src="login.js"></script>
- 
- </head>
- 
+<form name="blah" action="control/login.php">
+    //blah
 
+</form>
 
-?>
-
-<table width="980" border="0" align="center" cellpadding="0" cellspacing="0" id="tableLayoutMain">
-	  <tr>
-		<td id="headerBGHome"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-		  <tr>
-			<td width="152" align="center" valign="bottom"><a href="https://vancouver.ca/police/index.html"><img src="crest-lg-colour-tall.png" alt="Home - THE VANCOUVER POLICE DEPARTMENT" width="152" height="297" ></a></td>
-			<td align="left" valign="bottom">
-			 <div id="navUtility">
-				 <form>
-					USERNAME: <input type="text" name="username" >
-					</br>
-					PASSWORD: <input type="password" name="password" >
-					</br>
-					<input type="button" value="Login" onClick="Login(this.form);" >
-				 </form>
-			</div>
-			  <a href=""><img src="vpd-wordmark-tall.png" alt="Home - THE VANCOUVER POLICE DEPARTMENT | Beyond the Call" width="580" height="231" ></a>          </td>
-		  </tr>
-		  
-		</table></td>
-
-<?php
-
-include('footer.html');
- <tr>
-		<td id="footerBG"><img src="pixel-transparent.png" alt="-" width="1" height="29" ></td>
-	  </tr>
-	  <tr>
-		<td align="left" valign="top" id="footer"><a href="https://vancouver.ca/police/index.html"><img src="crest-sm-grey.png" alt="Vancouver Police Department" width="36" height="42" style="float:left; margin-right:4px;" ></a><span style="float:right;"><a href="#top">to top</a></span></br>
-		<span class="mousetype">Official Website of the Vancouver Police Peace Guard Department | &copy; <a href="http://www.sfu.ca/~zhongl/cmpt275/cmpt275groupe13.html">Peace Guard</a>, all rights reserved.</span></td>
-	  </tr>
-
-?>
+<?php include('footer.html'); ?>
