3de353f20aa835b84b02985f6c0e58bff8657dc3
T. Colin Strong <turvyc@gmail.com>
2013-02-14 14:08:03 -0800

    Added POST constants to login.php.

--- server/login.php
+++ server/login.php
@@ -1,8 +1,9 @@
 <?php include('header.html'); ?>
 
 <form name="login" action="control/login.control.php" method="POST">
-    USERNAME: <input type="text" name="username" > <br />
-    PASSWORD: <input type="password" name="password" > <br />
+    USERNAME: <input type="text" name="<?php echo _USERNAME; ?>" > <br />
+    PASSWORD: <input type="password" name="<?php echo _PASSWORD; ?>" > <br />
+    <input type="hidden" name="<?php echo _AGENT; ?>"
     <input type="submit" value="Login">
 </form>
 
