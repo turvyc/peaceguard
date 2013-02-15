e58b4f4197133f5c1d4c6281abe0322cc6956cf2
T. Colin Strong <cstrong@sfu.ca>
2013-02-14 20:18:19 -0800

    Added page header to login.php, as well as a couple of other little tweaks.

--- server/login.php
+++ server/login.php
@@ -1,9 +1,20 @@
-<?php include('header.html'); ?>
-<?php include('model/constants.model.php'); ?>
+<?php 
+
+/* login.php
+
+The login page. Unless they are already logged in, users will be automatically
+redirected to this page if they try to access any other page of the Website.
+
+*/
+
+include('header.html');
+include('model/constants.model.php');
+
+?>
 
 <form name="login" action="control/login.control.php" method="POST">
-    USERNAME: <input type="text" name="<?php echo _USERNAME; ?>" /> <br />
-    PASSWORD: <input type="password" name="<?php echo _PASSWORD; ?>" /> <br />
+    Username: <input type="text" name="<?php echo _USERNAME; ?>" /> <br />
+    Password: <input type="password" name="<?php echo _PASSWORD; ?>" /> <br />
     <input type="hidden" name="<?php echo _AGENT; ?>" value="<?php echo _WEBSITE; ?>" />
     <input type="submit" value="Login" />
 </form>
