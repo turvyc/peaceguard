e6a377cca03769294aa61ceb3adcdec9e53f1719
T. Colin Strong <cstrong@sfu.ca>
2013-02-17 12:35:24 -0800

    Added standard header to footer.php.

--- server/footer.php
+++ server/footer.php
@@ -1,3 +1,20 @@
+<?php
+
+/* footer.php
+
+The standard footer boilerplate for every page of the PeaceGuard website.
+
+Contributor(s): Kantar Lin, Colin Strong
+
+For a detailed list of changes to this file, enter the command `git log <file>` 
+or `git blame <file>`
+
+(c) 2013 Number 13 Developer's Group
+
+*/
+
+?>
+
                 </div> <!-- Closes content div -->
             </div> <!-- Closes body div -->
             <div id="footer">
