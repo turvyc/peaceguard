2e2361e235b79098fbe1c27c57f41a630a60bc24
T. Colin Strong <cstrong@sfu.ca>
2013-02-17 21:20:19 -0800

    Added standard file headers for the unit tests.

--- server/test/DataInterfaceTest.php
+++ server/test/DataInterfaceTest.php
@@ -1,5 +1,23 @@
 <?php
 
+/** DataInterfaceTest.php
+
+To run these unit tests, run `phpunit --bootstrap .bootstrap.php DataInterface.php`
+You need PEAR and PHPUnit installed on your system.
+
+These tests check pre-loaded rows in the database for the testing website, 
+located at http://peaceguard.dyndns.org:1728. Running these tests on any 
+other database will probably fail.
+
+Contributor(s): Colin Strong
+
+For a detailed list of changes to this file, enter the command `git log <file>` 
+or `git blame <file>`
+
+(c) 2013 Number 13 Developer's Group
+
+*/
+
 require_once('../model/datainterface.model.php');
 require_once('../model/constants.model.php');
 
--- server/test/HomePageTest.php
+++ server/test/HomePageTest.php
@@ -1,12 +1,26 @@
 <?php
 
-require_once('../model/session.model.php');
+/** HomePage.php
+
+To run these unit tests, run `phpunit --bootstrap .bootstrap.php HomePageTest.php`
+You need PEAR and PHPUnit installed on your system.
+
+These tests check pre-loaded rows in the database for the testing website, 
+located at http://peaceguard.dyndns.org:1728. Running these tests on any 
+other database will probably fail.
+
+Contributor(s): Colin Strong
+
+For a detailed list of changes to this file, enter the command `git log <file>` 
+or `git blame <file>`
+
+(c) 2013 Number 13 Developer's Group
+
+*/
+
 require_once('../model/volunteer.model.php');
 require_once('../model/constants.model.php');
 
-// These tests check pre-loaded rows in the database for the testing website, 
-// located at http://peaceguard.dyndns.org:1728. Running these tests on any 
-// other database will probably fail.
 class HomePageTest extends PHPUnit_Framework_TestCase {
 
     private $volunteer;
