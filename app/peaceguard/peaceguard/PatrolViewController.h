51383b4f807b44e30a6a78e22faf387757f356d1
Jonas Yao <junfuy@sfu.ca>
2013-02-27 21:06:15 -0800

    map feature demo

---
+++ app/peaceguard/peaceguard/PatrolViewController.h
@@ -0,0 +1,15 @@
+//
+//  PatrolViewController.h
+//  peaceguard
+//
+//  Created by JonasYao on 2013-02-27.
+//  Copyright (c) 2013 Ashley Lesperance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface PatrolViewController : UIViewController
+
+
+@end
