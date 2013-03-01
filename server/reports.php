3adf4a37925195357bc431e729b60e000fd53d0d
zhongl <zhongl@sfu.ca>
2013-02-28 16:49:14 -0800

    dropdown added

--- server/reports.php
+++ server/reports.php
@@ -21,12 +21,18 @@ checkLoggedIn($session);
 
 <h1>Incident Reports</h1>
 
-<p>
-Lorem ipsizzle dolizzle yo mamma , rizzle adipiscing elit. Nullizzle velizzle, fo shizzle volutpizzle, suscipit yippiyo, gravida vel, arcu. Pellentesque check it out fo shizzle my nizzle. Check it out erizzle. Go to hizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle dolor fizzle turpis tempizzle daahng dawg. Maurizzle owned nibh et fo. Things izzle we gonna chung. Pellentesque eleifend rhoncizzle nisi. In hizzle habitasse platea dictumst. I'm in the shizzle dapibizzle. Crackalackin bizzle urna, pretium pizzle, mattizzle doggy, eleifend vitae, nunc. Owned suscipizzle. Integer semper velit gizzle pizzle.
-</p>
-
-<p>
-Uhuh ... yih! for sure ass eget shizznit fo shizzle my nizzle pretizzle. Vivamus sizzle amizzle lacus. Pot own yo' nisl eget lacizzle auctizzle dang. Praesent suscipit viverra ipsizzle. Curabitur izzle arcu. Vestibulum enizzle enim, away yo, funky fresh eu, dignissim i saw beyonces tizzles and my pizzle went crizzle, libero. Nullizzle vitae pede nizzle i saw beyonces tizzles and my pizzle went crizzle posuere funky fresh. Quisque pede tortor, congue the bizzle, auctizzle a, bow wow wow sit ass, erizzle. Ass crackalackin dui. Aliquam gangsta fizzle, elementum doggy, sollicitudin izzle, fo imperdizzle, pot. Pimpin' a izzle eu mi shiznit vehicula. Mofo accumsan sagittizzle yo mamma. Bow wow wow pimpin' morbi yippiyo hizzle et break it down izzle malesuada famizzle ac turpizzle away. In boom shackalack. The bizzle elementizzle. Ut erizzle felizzle, semper the bizzle, suscipit izzle, crunk pulvinizzle, nisl. Nulla you son of a bizzle stuff sheezy.
-</p>
+<div id="dropdownMenu">
+	<select name="timeDropDown">
+	<option value="day">Today's Reports</option>
+	<option value="las30Days">Last Thirty Days' Reports</option>
+	<option value="year">Year To Date' Reports</option>
+	</select>
+	<select name="sortDropDown">
+	<option value="date">Sort By Date</option>
+	<option value="time">Sort By Time</option>
+	<option value="serverity">Sort By Severity</option>
+	<option value="username">Sort By Username</option>
+	</select>
+</div>
 
 <?php include('footer.php'); ?>
