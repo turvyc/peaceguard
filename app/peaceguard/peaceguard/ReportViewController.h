ca0112a474469264caa83d1917d88a5380d0c5ec
Jonas Yao <junfuy@sfu.ca>
2013-03-09 13:58:02 -0800

    some more variable for report overview

--- app/peaceguard/peaceguard/ReportViewController.h
+++ app/peaceguard/peaceguard/ReportViewController.h
@@ -30,6 +30,13 @@
 //-Data from pickerview
 @property (strong, nonatomic) NSString *incidentData;   //change to be consistent with server - same as severity
 @property (strong, nonatomic) NSString *severityData;
+
+//temp variable for overview
+@property (strong, nonatomic) NSString *incidentOverview;
+@property (strong, nonatomic) NSString *severityOverview;
+@property (strong, nonatomic) NSString *descriptionOverview;
+@property (strong, nonatomic) UIImage *imageOverview;
+
 //-open/close pickerview for incident type/severity
 - (IBAction)incidentButtonPressed:(id)sender;
 - (IBAction)severityButtonPressed:(id)sender;
