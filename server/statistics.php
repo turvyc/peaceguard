abebd752ed9fcd194a869e788ade6a1da4400c3b
zhongl <zhongl@sfu.ca>
2013-03-10 10:45:19 -0700

    dropdown menu created not funcitonal yet

--- server/statistics.php
+++ server/statistics.php
@@ -1,4 +1,4 @@
-<?php
+git<?php
 
 /* statistics.php
 
@@ -16,18 +16,28 @@ or `git blame <file>`
 */
 
 include('header.php');
+include('../control/patrolstats.php');
 checkLoggedIn($session);
 
 ?>
 
 <h1>Patrol Statistics</h1>
 
-<p>
-Lorem ipsizzle dolizzle yo mamma , rizzle adipiscing elit. Nullizzle velizzle, fo shizzle volutpizzle, suscipit yippiyo, gravida vel, arcu. Pellentesque check it out fo shizzle my nizzle. Check it out erizzle. Go to hizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle dolor fizzle turpis tempizzle daahng dawg. Maurizzle owned nibh et fo. Things izzle we gonna chung. Pellentesque eleifend rhoncizzle nisi. In hizzle habitasse platea dictumst. I'm in the shizzle dapibizzle. Crackalackin bizzle urna, pretium pizzle, mattizzle doggy, eleifend vitae, nunc. Owned suscipizzle. Integer semper velit gizzle pizzle.
-</p>
-
-<p>
-Uhuh ... yih! for sure ass eget shizznit fo shizzle my nizzle pretizzle. Vivamus sizzle amizzle lacus. Pot own yo' nisl eget lacizzle auctizzle dang. Praesent suscipit viverra ipsizzle. Curabitur izzle arcu. Vestibulum enizzle enim, away yo, funky fresh eu, dignissim i saw beyonces tizzles and my pizzle went crizzle, libero. Nullizzle vitae pede nizzle i saw beyonces tizzles and my pizzle went crizzle posuere funky fresh. Quisque pede tortor, congue the bizzle, auctizzle a, bow wow wow sit ass, erizzle. Ass crackalackin dui. Aliquam gangsta fizzle, elementum doggy, sollicitudin izzle, fo imperdizzle, pot. Pimpin' a izzle eu mi shiznit vehicula. Mofo accumsan sagittizzle yo mamma. Bow wow wow pimpin' morbi yippiyo hizzle et break it down izzle malesuada famizzle ac turpizzle away. In boom shackalack. The bizzle elementizzle. Ut erizzle felizzle, semper the bizzle, suscipit izzle, crunk pulvinizzle, nisl. Nulla you son of a bizzle stuff sheezy.
-</p>
+<form name="<?php echo _REPORT; ?>" action="control/patrolstats.control.php" method="POST">
+	<select name="<?php echo _TIME_PERIOD; ?>">
+		<option value="<?php echo _LAST_DAY; ?>">Today's Reports</option>
+		<option value="<?php echo _LAST_MONTH; ?>">Last Thirty Days' Reports</option>
+		<option value="<?php echo _LAST_YEAR; ?>">Last Year's Reports</option>
+		<option value="<?php echo _ALL_TIME; ?>">Year To Date' Reports</option>
+	</select>
+	
+	<select name="<?php echo _ORDER_BY; ?>">
+		<option value="<?php echo _NUMBER_VOLUNTEERS; ?>">Number of Registered Volunteers</option>
+		<option value="<?php echo _GLOBAL_STATISTICS; ?>">GLOBAL STATISTICS</option>
+		<option value="<?php echo _AVG; ?>">MEAN and MEDIAN</option>
+	</select>
+	<input type="hidden" name="<?php echo _AGENT; ?>" value="<?php echo _WEBSITE; ?>" />
+    <input type="submit" value="Submit" />
+</form>
 
 <?php include('footer.php'); ?>
