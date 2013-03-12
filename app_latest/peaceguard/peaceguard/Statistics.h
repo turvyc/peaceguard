a79fb88b71be59ebc3faf67e95a3e7f2f110a222
Jonas Yao <junfuy@sfu.ca>
2013-03-12 14:25:56 -0700

    patrol feature is complete

---
+++ app_latest/peaceguard/peaceguard/Statistics.h
@@ -0,0 +1,13 @@
+//
+//  Statistics.h
+//  peaceguard
+//
+//  Created by JonasYao on 2013-03-12.
+//  Copyright (c) 2013 Ashley Lesperance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Statistics : NSObject
+
+@end
---
+++ app_latest/peaceguard/peaceguard/Statistics.m
@@ -0,0 +1,13 @@
+//
+//  Statistics.m
+//  peaceguard
+//
+//  Created by JonasYao on 2013-03-12.
+//  Copyright (c) 2013 Ashley Lesperance. All rights reserved.
+//
+
+#import "Statistics.h"
+
+@implementation Statistics
+
+@end
