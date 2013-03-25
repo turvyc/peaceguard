57c2b6577b314a5f5eb10f3ba243c641f24ed8ce
Robert Sanchez <rcsanche@sfu.ca>
2013-03-25 02:09:39 -0700

    image resizing

--- app_latest/peaceguard/peaceguard/ReportViewController.m
+++ app_latest/peaceguard/peaceguard/ReportViewController.m
@@ -273,7 +273,7 @@
     
     [self dismissViewControllerAnimated:YES completion:nil];
     UIImage *image = info[UIImagePickerControllerOriginalImage];
-    [image drawInRect:CGRectMake(0, 0, 280, 163)];
+    [image drawInRect:CGRectMake(0, 0, 280, 150)];
     _imageView.image = image;
 
 }
